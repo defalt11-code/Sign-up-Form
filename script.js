d699e0a7bc0e6cb37daec9d9ad1b76e40653aaeb
defalt11-code <royilao1114@gmail.com>
2025-07-01 22:17:26 +0800

    remove the js validation in others input

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+//For animation when page load
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector(".wrapper").classList.add("animate");
 });
 
+//For form to confirm the password actually match
 const password = document.querySelector("#password");
 const confirmPassword = document.querySelector("#confirm-password");
 const form = document.querySelector("form");
