9127ca541ffddc5c4efbf3f94d3fae929dadc813
defalt11-code <royilao1114@gmail.com>
2025-07-01 19:30:14 +0800

    added a little javascript for confirm password So I can refresh my knowledge in Javascript

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector(".wrapper").classList.add("animate");
 });
+
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm-password");
+const form = document.querySelector("form");
+
+console.log(confirmPassword);
+
+form.addEventListener("submit", (e) => {
+  if (password.value !== confirmPassword.value) {
+    e.preventDefault();
+    confirmPassword.setCustomValidity("Passwords do not match");
+    confirmPassword.reportValidity();
+  } else {
+    confirmPassword.setCustomValidity("");
+  }
+});
